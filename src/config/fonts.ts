5a4eb8dea01452b77288390737c6135b825a103f
Alejandro Larreta Vzla <larreta_vzla@hotmail.com>
2024-01-28 18:35:45 -0700

    Avance hasta la Sección 14 - E-Commerce - Diseño - TesloShop. Clase 4: NextJS Fonts. Primer commit

---
+++ src/config/fonts.ts
@@ -0,0 +1,7 @@
+import { Inter, Montserrat_Alternates } from "next/font/google";
+
+export const inter = Inter({ subsets: ["latin"] });
+export const titleFont = Montserrat_Alternates({
+  subsets: ["latin"],
+  weight: ["400","500", "600", "700"]
+})
